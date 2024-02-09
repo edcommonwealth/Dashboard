d9ff6252b6fba42550641ab89c8fbcbd6380ec97
Nelson Jovel <memoryman51@hotmail.com>
2024-02-09 15:43:27 -0800

    chore: move unique index to end of statement to test if it gets properly defined in host application

--- db/migrate/20240103232412_create_dashboard_academic_years.rb
+++ db/migrate/20240103232412_create_dashboard_academic_years.rb
@@ -6,6 +6,6 @@ class CreateDashboardAcademicYears < ActiveRecord::Migration[7.1]
       t.timestamps
     end
 
-    add_index :dashboard_academic_years, :range, unique: true, name: "dashboard_range"
+    add_index :dashboard_academic_years, :range, name: "dashboard_range", unique: true
   end
 end
